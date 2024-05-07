cffc39bc12ea3528d84df9b61cd0928cf4a2a069
Zack <zach@ridenourfamily.com>
2024-05-06 20:09:46 -0600

    I add a few more things to the lists and changed the text boxes

--- src/list2.ts
+++ src/list2.ts
@@ -3,62 +3,62 @@ import { Item } from "./types";
 export const list2: Item[] = [
   {
     name: "Waste of good napkin",
-    price: 20,
+    price: 300,
     description: "(A coupon that is 25% off of your next purchase anywhere only works once)",
   },
   {
     name: "Snack?",
-    price: 20,
+    price: 100,
     description: "(Some thrown out food found in a dumpster)",
   },
   {
     name: "Goblin hand sanitizer",
-    price: 20,
+    price: 200,
     description: "(An unopen jar of Peanut Butter)",
   },
   {
     name: "Dino!",
-    price: 20,
+    price: 300,
     description: "(Dinosaur nuggets with your choice of dipping sauce",
   },
   {
     name: "Garbage Crown!",
-    price: 20,
+    price: 200,
     description: "(A Burger King crown that will stay on your head until you take it off)",
   },
   {
     name: "Disappointment",
-    price: 20,
+    price: 100,
     description: "(A batch of perfectly baked cookies but the goblins don't have any milk to go with it)",
   },
   {
     name: "Soup?",
-    price: 20,
+    price: 100,
     description: "(A soggy bowl of ceral)",
   },
   {
     name: "Hobo Gum",
-    price: 20,
+    price: 200,
     description: "(A rock, small enough to chew on)",
   },
   {
     name: "Terrible Seasonal Cookie Thief",
-    price: 20,
+    price: 200,
     description: "(Santa's hat)",
   },
   {
     name: "The One True Enemy",
-    price: 20,
+    price: 100,
     description: "(A bar of soap with a bite taken out of it?)",
   },
   {
     name: "Last weak fish trip",
-    price: 20,
+    price: 200,
     description: "(six left boots, three fish, one fishing pole with a pair of pants on the hook, & tombstone)",
   },
   {
     name: "Holly's Grail",
-    price: 10,
+    price: 200,
     description: "(Fresh costco rotisserie chicken, probably stolen from a person named Holly becuase her receit is still in the bag)",
   },
 ];
