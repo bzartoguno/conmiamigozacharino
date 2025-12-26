ed7d699bd76b2430056bd047c3ffc8e0fd031693
Shawn Ridenour <zartog@hotmail.com>
2025-12-25 21:31:29 -0700

    Add Supreme Smithy specific styling

--- src/SupremeSmithy.tsx
+++ src/SupremeSmithy.tsx
@@ -1,12 +1,14 @@
 import { ShopTemplate } from "./ShopTemplate";
 import { tribeSupremeSmithy } from "./tribeSupremeSmithy";
 import supremeSmithyBackground from "./Supreme Smithy.png";
+import styles from "./SupremeSmithy.module.css";
 
 export function SupremeSmithy({ onBack }: { onBack?: () => void }) {
   return (
     <ShopTemplate
       tribe={tribeSupremeSmithy}
       backgroundImage={supremeSmithyBackground}
+      styles={styles}
       onBack={onBack}
     />
   );
