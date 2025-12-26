4d91c7a68d998fe31cfea811c0c3efc48ae1fee7
Shawn Ridenour <zartog@hotmail.com>
2025-12-25 21:26:11 -0700

    Add themed styling for O-Papies Oracle Readings

--- src/OPapiesOracleReadings.tsx
+++ src/OPapiesOracleReadings.tsx
@@ -1,12 +1,14 @@
 import { ShopTemplate } from "./ShopTemplate";
 import { tribeOPapiesOracleReadings } from "./tribeOPapiesOracleReadings";
 import oPapiesBackground from "./O Papies Oracle Readings.png";
+import styles from "./OPapiesOracleReadings.module.css";
 
 export function OPapiesOracleReadings({ onBack }: { onBack?: () => void }) {
   return (
     <ShopTemplate
       tribe={tribeOPapiesOracleReadings}
       backgroundImage={oPapiesBackground}
+      styles={styles}
       onBack={onBack}
     />
   );
