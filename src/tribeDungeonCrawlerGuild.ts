ce841c28f16bab54b069ea7ddc8149299d99865c
Zach <zach@ridenourfamily.com>
2025-12-30 15:55:11 -0700

    Started workin on red shops

--- src/tribeDungeonCrawlerGuild.ts
+++ src/tribeDungeonCrawlerGuild.ts
@@ -5,27 +5,47 @@ export const tribeDungeonCrawlerGuild: Tribe = {
   owner: "Doug",
   percentAngry: 0,
   priceVariability: 5,
-  insults: [""],
+  insults: ["ba", "da", "boo"],
   items: [
     {
       name: "Basic Information about a Dungeon",
+      description: "Provides basic details about a dungeon, including its layout, dungeon gimmicks, and potential hazards.",
       price: 10,
     },
     {
       name: "Retrieval Team",
+      description: "Pay before you enter the dungeon. If you don't return after a set amount of time we'll send a team to retrieve you. Whether you get lost or make have kicked the bucket.",
       price: 20,
     },
     {
       name: "Premium Membership (10% less tax)",
+      description: "Grants you a 10% discount on all dungeon-related services and items we also take about 10% less tax on your loot. And we can send you a monthly newsletter about nearby dungeons in your area!",
       price: 50,
     },
     {
-      name: "Escort Quest (50% less tax)",
+      name: "Tutor guide (50% less tax)",
+      description: "Not everyone who enters a dungeon is in it for the slaughter. Some people just want to explore and have fun. With this service, we assign you a guide member who will accompany you through the dungeon, ensuring your safety. Plus, we take 50% less tax on any loot you find!",
       price: 100,
     },
     {
-      name: "Premium Plus Membership (Tax-free)",
+      name: "Ultra Membership (Tax-free)",
+      description: "Our ultimate package for the true dungeon enthusiast. Enjoy tax-free status on all your dungeon loot and services. Additionally, you'll receive exclusive access to members-only dungeons and events throughout the year. Tho will need to occationally babysit people who want to be escorted through a dungeon.",
       price: 1000,
+    },
+    {
+      name: "Greeting Adventurers!",
+      description: "We welcome you to this public dungeon! Feel free to explore and have fun. Though we will have to tax any loot you find here. Becuase we put that money back into maintaining the dungeon and making sure that it doesn't spawn more monsters cuasing an outbreak in the nearby town. Any money left over from that we put towards setting up new adventures like yourself!",
+      price: 0,
+    },
+    {
+      name: "Sack of Sand",
+      description: "Well it's a sack of sand. We feel bad charging for it but it's surpisingly useful for setting off traps early or weighing down pressure plates for puzzles.",
+      price: 2,
+    },
+    {
+      name: "Cooking Kit",
+      description: "It's basic cooking kit for preparing meals in almost any environment. Includes a small pot, pan, utensils, basic spices, and a little booklet on how to prep and cook monsters you can find in dungeons.",
+      price: 7,
     },
   ],
 };
--- src/tribeFindAFriend.ts
+++ src/tribeFindAFriend.ts
@@ -5,37 +5,22 @@ export const tribeFindAFriend: Tribe = {
   owner: "Fran",
   percentAngry: 0,
   priceVariability: 0,
-  insults: [""],
+  insults: ["Fun Fact: This is where Zach puts his charater concepts that he never really play, but thinks are cool."],
   items: [
     {
-      name: "Amber",
+      name: "Amber (Kalashtar)",
       price: 0,
-      description: "Kalashtar, Appreciate",
+      description: "Amber is a girl failure. Due to her some 'family' history and clumsy nature she has picked up a few minor psionic abilities like telepathy and a bit of clairvoyance.",
     },
     {
-      name: "Dandelion Duchess",
+      name: "Jade (Teoran)",
       price: 0,
-      description: "Floral Dragon, Bard",
+      description: "Jade is a quite person who prefers to stay in the background. He doesn't like to talk at all he'll just nod or shake his head. Though he does have a knack for being in the right place at the right time for protecting his charges.",
     },
     {
-      name: "Ebony",
+      name: "Johnny boi (Dwarf)",
       price: 0,
-      description: "Dark Elf, Thief",
-    },
-    {
-      name: "Gwain",
-      price: 0,
-      description: "Human V, Feruchemist",
-    },
-    {
-      name: "Jade",
-      price: 0,
-      description: "Teoran, Bodyguard",
-    },
-    {
-      name: "John Jon",
-      price: 0,
-      description: "Dwarf, Monster Ecologist",
+      description: "Johnny is an odd fellow, he likes monsters more then people. He'll ramble on for hours on just about any monster and how they evolved, what they eat, and how they hunt. He is a bit socially awkward but means well.",
     },
     {
       name: "Kip",
@@ -46,16 +31,6 @@ export const tribeFindAFriend: Tribe = {
       name: "Phil & Bill",
       price: 0,
       description: "Halfling, Henchmen",
-    },
-    {
-      name: "Swaine",
-      price: 0,
-      description: "Human V, Radiant Knight",
-    },
-    {
-      name: "Twain",
-      price: 0,
-      description: "Human V, Allomancer",
     },
     {
       name: "Warlock Homes",
